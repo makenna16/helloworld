d29a8512a3bf385b7b1d70ded2aaa024b54bc5c8
makenna <makenna.gordon.2023@student.lhschools.org>
2020-03-04 13:06:35 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Makenna");
+
+
+        System.out.println("I went to King's island.");
+        System.out.println("I also went to Zoombezi Bay.");
+        System.out.println("And I went to Maryland.");
+    }
+}
